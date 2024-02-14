b27f6c27897c18c36a134ca5573a5ee2eabba184
tin <a921213f@gmail.com>
2024-02-15 00:54:31 +0800

    .

--- lib/view_model/user_view_model.dart
+++ lib/view_model/user_view_model.dart
@@ -3,12 +3,12 @@ import 'package:flutter_application_template/model/user.dart';
 import 'package:flutter_application_template/repo/user_repo.dart';
 import 'package:flutter_application_template/view_model/google.dart';
 
-class UserModel with ChangeNotifier {
+class UserViewModel with ChangeNotifier {
   String? _uid;
   List<TodoItem>? _todoList;
   List<String>? _folders;
 
-  UserModel(GoogleViewModel vm) {
+  UserViewModel(GoogleViewModel vm) {
     _uid = vm.user?.uid;
     if (_uid != null) {
       UserRepo.read(_uid!).then((value) {
